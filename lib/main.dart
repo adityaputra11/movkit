a91cc8108927becc748d773ead329b4cfb1b510c
aditya putra pratama <adit@dreite.co.id>
2018-10-22 22:26:58 +0700

    fixing Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
-import 'package:movkit/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:movkit/screens/movies.dart';
+import 'package:movkit/screens/detail.dart';
 
-void main(){
-  new Routes();
+void main() {
+  runApp(MaterialApp(
+    title: 'Named Routes Demo',
+    // Start the app with the "/" named route. In our case, the app will start
+    // on the FirstScreen Widget
+    initialRoute: '/',
+    routes: {
+      // When we navigate to the "/" route, build the FirstScreen Widget
+      '/': (context) => MovieScreen(),
+      // When we navigate to the "/second" route, build the SecondScreen Widget
+      '/detail': (context) => DetailScreen(),
+    },
+    theme: ThemeData.dark(),
+  ));
 }
--- lib/screens/movies.dart
+++ lib/screens/movies.dart
@@ -1,62 +1,48 @@
 import 'package:flutter/material.dart';
 
 
-void main() => runApp(new MovieApp());
+void main()=>runApp(MovieScreen());
 
-class MovieApp extends StatelessWidget {
+
+class MovieScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text("Aditya Putra Pratama"),
+              new Text("UX/UI Developer"),
+              new Text("adityaputrapratama39@gmail.com"),
+              new Text("github.com/adityaputra11")
+            ],
+          ),
+          decoration: BoxDecoration(
+            color: Colors.green
+          ),
+        ),
       ),
-      home: new MoviePage(title: 'Kit'),
+      appBar: AppBar(
+        title: Text('KIT'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new RaisedButton(
+            child: Text('Movie screen'),
+            onPressed: () {
+            // Navigate to the second screen using a named route
+            Navigator.pushNamed(context, '/detail');
+          },
+        ),
+         ],
+       ),
+      ),
     );
   }
 }
 
-class MoviePage extends StatefulWidget {
-  MoviePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MoviePageState createState() => new _MoviePageState();
-}
-class _MoviePageState extends State<MoviePage> {
-  //final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  Widget drawerSection =  Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          new Text("Aditya Putra pratama"),
-          new Text("UX/UI Designer"),
-          new Text("adityaputrapratama39@gmail.com"),
-          new Text("https://github.com/adityaputra11")
-        ],
-      ),
-  );
-  
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      key: _scaffoldKey,
-      appBar: new AppBar(
-        title: new Text(widget.title),
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            drawerSection
-          ],
-        ),
-        ),
-      body: new Center(
-        child:new RaisedButton(onPressed: (){Navigator.pushNamed(context, "/series");}, child: const Text("Series")),
-      ),
-      );
-
-  }
-}
